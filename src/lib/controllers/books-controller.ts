85d596818cca913955c5aaf046a40cd3de3c16a2
Obed Nuertey <onuertey1997@gmail.com>
2024-10-22 17:35:14 +0000

    add: added getAllBooks module to BooksController class

--- src/lib/controllers/books-controller.ts
+++ src/lib/controllers/books-controller.ts
@@ -4,5 +4,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default class BooksController {
     constructor(){};
 
+    public async getAllBooks(req: NextApiRequest, res: NextApiResponse) {
+        console.log("This route will provide all the books");
+        let books:any;
+
+        try{
+            books = await Book.find();
+        }catch(e){
+            console.error(e);
+        }
+        if(!books){
+            return res.status(404).json({message: "No books found"});
+        }
+        return res.status(200).json(books);
+    }
+
     
 }
