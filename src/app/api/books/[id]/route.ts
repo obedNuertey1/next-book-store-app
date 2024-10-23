0cfdcb04c7e4bf67fea01c086619e3ded7bc6e32
Obed Nuertey <onuertey1997@gmail.com>
2024-10-23 13:44:20 +0000

    feat: added GET function/route to @/app/api/books/[id].ts

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -1,3 +1,10 @@
 import BookController from "@/lib/controllers/books-controller";
 import {NextRequest} from "next/server";
 import {MongooseConnect} from "@/lib/mongoose";
+
+const db = new MongooseConnect(process.env.MONGO_URI!);
+const Book = new BookController(db);
+
+export async function GET(req: NextRequest, {params}: {params: {id: string}}) {
+    return await Book.getBookById(req, {params});
+}
