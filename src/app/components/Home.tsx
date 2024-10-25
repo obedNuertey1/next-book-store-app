1bf87e844203e90d5fcd8aa17a6ffa14aa1761fa
Obed Nuertey <onuertey1997@gmail.com>
2024-10-25 00:44:32 +0000

    patch: removed Home.name='Home'; scope=src/app/components/Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -2,12 +2,12 @@
 import { Box, Button, Typography } from "@mui/material";
 import Link from "next/link";
 
-const Home = () => {
+const Home = async () => {
   return (
     <div>
       <Box display="flex" flexDirection="column" alignItems="center">
         {/* @ts-ignore */}
-        <Button LinkComponent={Link} to={"/books"} sx={{mt: 15, background: "orangered"}} variant="contained">
+        <Button LinkComponent={Link} href={"/books"} sx={{mt: 15, background: "orangered"}} variant="contained">
           <Typography>VIEW ALL BOOKS</Typography>
         </Button>
       </Box>
@@ -15,6 +15,5 @@ const Home = () => {
   );
 };
 
-Home.name = "Home";
 
 export default Home;
