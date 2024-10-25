f0c1fec99fc2ec3da49ecd5e8ca665ee8d705524
Obed Nuertey <onuertey1997@gmail.com>
2024-10-25 00:51:19 +0000

    patch: converted file to 'use server' and rendered HomePage component; scope=src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
-"use client";
-import {Box, Typography} from "@mui/material";
-import {useEffect, useState} from "react";
-
-export default function Home() {
-  const [books, setBooks] = useState([]);
-  useEffect(() => {
-    (async ()=>{
-      const res = await fetch("/api/books");
-      if(!res.ok){
-        return;
-      }
-      const data = await res.json();
-      console.log({data});
-    })()
-  }, []);
-  return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Typography variant="h1" component="h1" gutterBottom>
-          Welcome to Next.js!
-        </Typography>
-      </Box>
-    </>
-  );
+"use server";
+import HomePage from "./components/Home";
+export default async function Home() {
+  return <HomePage />;
 }
